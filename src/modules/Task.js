cb5205f327fc0b0fdf96872a93c655a4a6bc0569
ISIKALU01 <davisleevtobi@gmail.com>
2023-01-06 09:42:28 +0100

    add task class

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,2 +1,42 @@
-export default class Task {}
+export default class Task {
+    constructor(name, dueDate = 'No date', priority, description) {
+      this.name = name
+      this.dueDate = dueDate
+      this.priority = priority
+      this.description = description
+    }
+  
+    setName(name) {
+      this.name = name
+    }
+  
+    getName() {
+      return this.name
+    }
+  
+    setDate(dueDate) {
+      this.dueDate = dueDate
+    }
+  
+    getDate() {
+      return this.dueDate
+    }
+
+    setPriority(priority){
+        this.priority = priority
+    }
+
+    getPriority() {
+        return this.priority 
+    }
+
+    setDescription(description){
+        this.description = description
+    }
+
+    getDescription(){
+        return this.description
+    }
+}
+
   
